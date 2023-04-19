7ca34f4fadbfef53ebb71cd8de55b654ed09664a
BoBaHPyt <vovashokop@gmail.com>
2023-04-19 17:18:05 +0700

    bind to google sheets

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ def export_to_google_sheets(df, crenditals, sheet_id, worksheet_id):
     crenditals = ServiceAccountCredentials.from_json_keyfile_name(crenditals, scope)
     client = gspread.authorize(crenditals)
     
-    sheet = client.open_by_key(sheet_id)
+    sheet = client.open(sheet_id)
     worksheet = sheet.get_worksheet(worksheet_id)
-    sheet.values_clear(f"{worksheet.title}!A2:E10000")
+    #sheet.values_clear(f"{worksheet.title}!A2:E10000")
     worksheet.update(df)
 
 
@@ -24,6 +24,6 @@ async def main():
         await tp.login("zakupki@best-coll.ru", "Collection1")
         products += await tp.get_all_products()
     products[1].append(datetime.datetime.now().strftime("%d.%m.%Y %H:%M"))
-    export_to_google_sheets([] + products, "gapi.json", "1sgVk3nJTAc6ihG16SFWiRlCWUTFTF6bhHu1c8G8yDhk", 0)
+    export_to_google_sheets(products, "gapi.json", "1FS_5-Q4fMCBDGKGoGCQ3MsEYlHCOwmLtTUmkIdvQ66A", "Парсер остатков с сайта")
 
 asyncio.run(main())
